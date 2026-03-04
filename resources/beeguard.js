e61e6b22e029fa4a43714b6d23af9538179a9da1
Smilos71 <147918915+Smilos71@users.noreply.github.com>
2026-03-04 19:51:08 +0100

    Update beeguard.js

--- resources/beeguard.js
+++ resources/beeguard.js
@@ -1,76 +1,66 @@
-/* @--*#             +.::+                                        
-                                      @::---=           %:-:-:@                                      
-                                      %----+             %-:-@                                       
-                                        +* @             @                                          
-                                             @           @%                                          
-                                             @@#*****++*%@#                                          
-                                          %#%************%**@               %                        
-                             %%*****%@%##****#************#**@            #%                         
-                            =*#%.....-#**#+***#+***#*******+***%          @*@*@@@* @*#:........#**%*%-#****##*****++*****@       %%#***###*%+                 
-                           #%=........*-%**%::-*%***=%***#****************@@@@#   @                 
-                          %**:.......:===*#*:**:*+#*%:*@*#%**%*******************%#                  
-                           #%-........==+**%##*-:::::+#%*::+%#***%**********###****@                 
-                           @*#:.......*=#%#..*%-::::=--%.-*:**%*****###***********@@                 
-                          #.**%+:=....:###=:%*%:::::.%%%=.@-***:###*****%%*+*%@%%#*@                 
-                          +...%*#@%%#**#*@::.:::=::::-%=.::%*+=-:%##%+--====--*@ %##                 
-                          #......:%%%:@#%#::::::::::::::::-::==:+*-===-==-=+===## @                  
-                           #....#:::#*.-@%+:::::*=:::::::::*@*%+====+:=====++=%****%                 
-                            @...*:::*:*.==%#%-::::::::::-%#%=======@:#+====* @#@%                    
-                              +.*:::=*+-=.-+%%%-=%++%=+##%-===--==+:--=#==-+%                        
-                                %+::%++%#%*=%---*#=+**--=#+=-==#%+===%-*+######%@                    
-                                 %%**#%%-##-%%%%#*+==++#-%#:%%@-#%--%:-@                             
-                                  %#%%#--%*=---:-+*###*+%%-+%%%*-#%#-* @@+--*%#=#%%%%%#+=---+@%-#%-+%+--%%%                               
-                                    @%@%@@=------+#%%%%%%#:#%--*%*-@                                 
-                                         %%%%%%%%+-------@%*%#:-#%                                   
-                                        %+@@@%+=*#%%%%%%%@ %@@@%                                     
-                                        *=-#=--=%%%#++*%@%                                           
-                                      %#%#%*+*%%#---=+=-=-@                                          
-                                      %#*+*###=-#%%##%#%%%*+                                         
-                                       %%@%::::=#---+*+---%%                                         
-                                           -:::* =%##%@@* @+:+*%::::%                                               
-                                           @%%@ %:::%                                                
-                                           @%%@ %::+                                                 
-                                              @%=-%@                                                 
-                                              %###@                                                  
-                                              %##%                                                   
-                                              @#%#                                                   
-                                             +###%                                                   
-                                              @%%* ____             _             _            _   _            
-                 |  _ \ _ __ ___ | |_ ___  ___| |_ ___  __| | | |__  _   _ 
-                 | |_) | '__/ _ \| __/ _ \/ __| __/ _ \/ _` | | '_ \| | | |
-                 |  __/| | | (_) | ||  __/ (__| ||  __/ (_| | | |_) | |_| |
-                 |_|__ |_|  \___/ \__\___|\___|\__\___|\__,_| |_.__/ \__, |
-                 | __ )  ___  ___ / ___|_   _  __ _ _ __ __| |       |___/ 
-                 |  _ \ / _ \/ _ \ |  _| | | |/ _` | '__/ _` |             
-                 | |_) |  __/  __/ |_| | |_| | (_| | | | (_| |             
-                 |____/ \___|\___|\____|\__,_|\__,_|_|  \__,_|             
+/*                                                                                                
+                                                                                                    
+                                      @--*#            +.::+                                        
+                                     @::---=          %:-:-:@                                       
+                                     %----+            %-:-@                                        
+                                       +*  @             @                                          
+                                            @           @%                                          
+                                            @@#*****++*%@#                                          
+                                         %#%************%**@             %                          
+                             %%*****%@%##****#************#**@           #%                         
+                           =*#%.....-#**#+***#+***#*******+***%         @*@*@@@*                    
+                          @*#:........#**%*%-#****##*****++*****@     %%#***###*%+                  
+                          #%=........*-%**%::-*%***=%***#****************@@@@#   @                  
+                         %**:.......:===*#*:**:*+#*%:*@*#%**%*******************%#                  
+                          #%-........==+**%##*-:::::+#%*::+%#***%**********###****@                 
+                          @*#:.......*=#%#..*%-::::=--%.-*:**%*****###***********@@                 
+                         #.**%+:=....:###=:%*%:::::.%%%=.@-***:###*****%%*+*%@%%#*@                 
+                         +...%*#@%%#**#*@::.:::=::::-%=.::%*+=-:%##%+--====--*@ %##                 
+                         #......:%%%:@#%#::::::::::::::::-::==:+*-===-==-=+===## @                  
+                          #....#:::#*.-@%+:::::*=:::::::::*@*%+====+:=====++=%****%                 
+                           @...*:::*:*.==%#%-::::::::::-%#%=======@:#+====* @#@%                    
+                             +.*:::=*+-=.-+%%%-=%++%=+##%-===--==+:--=#==-+%                        
+                               %+::%++%#%*=%---*#=+**--=#+=-==#%+===%-*+######%@                    
+                                %%**#%%-##-%%%%#*+==++#-%#:%%@-#%--%:-@                             
+                                 %#%%#--%*=---:-+*###*+%%-+%%%*-#%#-*                               
+                                 @@+--*%#=#%%%%%#+=---+@%-#%-+%+--%%%                               
+                                   @%@%@@=------+#%%%%%%#:#%--*%*-@                                 
+                                        %%%%%%%%+-------@%*%#:-#%                                   
+                                       %+@@@%+=*#%%%%%%%@ %@@@%                                     
+                                       *=-#=--=%%%#++*%@%                                           
+                                     %#%#%*+*%%#---=+=-=-@                                          
+                                     %#*+*###=-#%%##%#%%%*+                                         
+                                      %%@%::::=#---+*+---%%                                         
+                                          -:::* =%##%@@*                                            
+                                          @+:+*%::::%                                               
+                                          @%%@ %:::%                                                
+                                          @%%@ %::+                                                 
+                                              @%=-%@                                                
+                                              %###@                                                 
+                                              %##%                                                  
+                                             @#%#                                                   
+                                            +###%                                                   
+                                             @%%*                                                   
+                                                                                                    
+                                                                                                    
+                 ____            _            _           _   _           
+                |  _ \ _ __ ___ | |_ ___  ___| |_ ___  __| | | |__  _   _ 
+                | |_) | '__/ _ \| __/ _ \/ __| __/ _ \/ _` | | '_ \| | | |
+                |  __/| | | (_) | ||  __/ (__| ||  __/ (_| | | |_) | |_| |
+                |_|__ |_|  \___/ \__\___|\___|\__\___|\__,_| |_.__/ \__, |
+                | __ )  ___  ___ / ___|_   _  __ _ _ __ __| |       |___/ 
+                |  _ \ / _ \/ _ \ |  _| | | |/ _` | '__/ _` |             
+                | |_) |  __/  __/ |_| | |_| | (_| | | | (_| |             
+                |____/ \___|\___|\____|\__,_|\__,_|_|  \__,_|             
+
+                                                                                         
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+
+
 
 */
 
-function _0xc24a(_0xedb9d5,_0xd0bbd9){_0xedb9d5=_0xedb9d5-0x1c2;const _0x4f5f92=_0x4f5f();let _0xc24a19=_0x4f5f92[_0xedb9d5];return _0xc24a19;}(function(_0x2074f2,_0x17ea78){const _0xa612fe=_0xc24a,_0x515500=_0x2074f2();while(!![]){try{const _0x1a3299=parseInt(_0xa612fe(0x1e1))/0x1+parseInt(_0xa612fe(0x1c9))/0x2*(parseInt(_0xa612fe(0x1cb))/0x3)+-parseInt(_0xa612fe(0x1de))/0x4*(parseInt(_0xa612fe(0x1cf))/0x5)+-parseInt(_0xa612fe(0x1c5))/0x6+parseInt(_0xa612fe(0x1d4))/0x7*(parseInt(_0xa612fe(0x1e0))/0x8)+-parseInt(_0xa612fe(0x1d2))/0x9*(parseInt(_0xa612fe(0x1d0))/0xa)+-parseInt(_0xa612fe(0x1c6))/0xb*(-parseInt(_0xa612fe(0x1dd))/0xc);if(_0x1a3299===_0x17ea78)break;else _0x515500['push'](_0x515500['shift']());}catch(_0x5f195e){_0x515500['push'](_0x515500['shift']());}}}(_0x4f5f,0x91300),(function(){const _0x399230=_0xc24a,_0x2536c7=_0x399230(0x1e3);function _0xa7c065(){const _0x3bda29=_0x399230;document[_0x3bda29(0x1db)][_0x3bda29(0x1d5)]['overflow']='hidden';const _0x5a54c6=document[_0x3bda29(0x1ce)]('div');_0x5a54c6['id']=_0x3bda29(0x1d7),_0x5a54c6[_0x3bda29(0x1d5)][_0x3bda29(0x1c7)]=_0x3bda29(0x1d8);const _0x50c1e6=document[_0x3bda29(0x1ce)](_0x3bda29(0x1df));_0x50c1e6[_0x3bda29(0x1cd)]=_0x2536c7,_0x50c1e6[_0x3bda29(0x1d5)][_0x3bda29(0x1c7)]=_0x3bda29(0x1ca),_0x5a54c6['appendChild'](_0x50c1e6),document[_0x3bda29(0x1db)][_0x3bda29(0x1c3)](_0x5a54c6);
-
-// --- UNIVERZÁLNÍ MELI OCHRANA (OPRAVENÁ PRO VERCEL A GITHUB) ---
-window['addEventListener'](_0x3bda29(0x1c2),function(e){
-    const _0x51558a=_0x3bda29;
-    if(e[_0x51558a(0x1d3)] && e[_0x51558a(0x1d3)][_0x51558a(0x1cc)]===_0x51558a(0x1d6)){
-        
-        const origin = e.origin || "";
-        // Teď povolujeme všechno z Endory, Vercelu i GitHubu
-        const isAllowed = origin.includes("endora.site") || 
-                          origin.includes("vercel.app") || 
-                          origin.includes("is-a.dev");
-
-        if(!isAllowed){
-            e['stopImmediatePropagation']();
-            document['documentElement']['innerHTML']='<body style="background:#0a0a0b;margin:0;display:flex;align-items:center;justify-content:center;height:100vh;flex-direction:column;font-family:sans-serif;"><img src="https://i.ibb.co/cKFtNWF7/fuckyou.png" style="width:300px;"><h1 style="color:red;font-size:3rem;margin-top:20px;">FUCK YOU</h1><p style="color:#444;">Origin blocked: '+origin+'</p></body>';
-            return;
-        }
-        
-        _0xea631e(_0x5a54c6);
-    }
-},!![]);}
-
-function _0xea631e(_0x309dfd){const _0x49c200=_0x399230;_0x309dfd[_0x49c200(0x1d5)][_0x49c200(0x1dc)]='opacity\x200.6s\x20ease',_0x309dfd['style'][_0x49c200(0x1e2)]='0',document[_0x49c200(0x1db)]['style'][_0x49c200(0x1c4)]='',setTimeout(()=>{_0x309dfd['remove']();},0x258);}
-document[_0x399230(0x1da)]===_0x399230(0x1d9)||document['readyState']===_0x399230(0x1d1)?_0xa7c065():document['addEventListener'](_0x399230(0x1c8),_0xa7c065);}()));
-
-function _0x4f5f(){const _0x541a7b=['src','createElement','25tmcKfc','4960iJEkTf','interactive','3330ufPCHq','data','7UyZrWn','style','beeguard-success','beeguard-overlay','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20position:\x20fixed;\x20top:\x200;\x20left:\x200;\x20width:\x20100%;\x20height:\x20100%;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20background:\x20#0c0c0c;\x20z-index:\x209999999;\x20border:\x20none;\x0a\x20\x20\x20\x20\x20\x20\x20\x20','complete','readyState','body','transition','348YquIoI','905124aqyNHt','iframe','7958584HeCclD','220975bEnxNl','opacity','https://smilos.is-a.dev/resources/beeguardframe.html','message','appendChild','overflow','3500208gRpasr','101035BBWcOY','cssText','DOMContentLoaded','2ruuRvc','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20width:\x20100%;\x20height:\x20100%;\x20border:\x20none;\x0a\x20\x20\x20\x20\x20\x20\x20\x20','3032454bzsijv','type'];_0x4f5f=function(){return _0x541a7b;};return _0x4f5f();}
+function _0xc24a(_0xedb9d5,_0xd0bbd9){_0xedb9d5=_0xedb9d5-0x1c2;const _0x4f5f92=_0x4f5f();let _0xc24a19=_0x4f5f92[_0xedb9d5];return _0xc24a19;}(function(_0x2074f2,_0x17ea78){const _0xa612fe=_0xc24a,_0x515500=_0x2074f2();while(!![]){try{const _0x1a3299=parseInt(_0xa612fe(0x1e1))/0x1+parseInt(_0xa612fe(0x1c9))/0x2*(parseInt(_0xa612fe(0x1cb))/0x3)+-parseInt(_0xa612fe(0x1de))/0x4*(parseInt(_0xa612fe(0x1cf))/0x5)+-parseInt(_0xa612fe(0x1c5))/0x6+parseInt(_0xa612fe(0x1d4))/0x7*(parseInt(_0xa612fe(0x1e0))/0x8)+-parseInt(_0xa612fe(0x1d2))/0x9*(parseInt(_0xa612fe(0x1d0))/0xa)+-parseInt(_0xa612fe(0x1c6))/0xb*(-parseInt(_0xa612fe(0x1dd))/0xc);if(_0x1a3299===_0x17ea78)break;else _0x515500['push'](_0x515500['shift']());}catch(_0x5f195e){_0x515500['push'](_0x515500['shift']());}}}(_0x4f5f,0x91300),(function(){const _0x399230=_0xc24a,_0x2536c7=_0x399230(0x1e3);function _0xa7c065(){const _0x3bda29=_0x399230;document[_0x3bda29(0x1db)][_0x3bda29(0x1d5)]['overflow']='hidden';const _0x5a54c6=document[_0x3bda29(0x1ce)]('div');_0x5a54c6['id']=_0x3bda29(0x1d7),_0x5a54c6[_0x3bda29(0x1d5)][_0x3bda29(0x1c7)]=_0x3bda29(0x1d8);const _0x50c1e6=document[_0x3bda29(0x1ce)](_0x3bda29(0x1df));_0x50c1e6[_0x3bda29(0x1cd)]=_0x2536c7,_0x50c1e6[_0x3bda29(0x1d5)][_0x3bda29(0x1c7)]=_0x3bda29(0x1ca),_0x5a54c6['appendChild'](_0x50c1e6),document[_0x3bda29(0x1db)][_0x3bda29(0x1c3)](_0x5a54c6),window['addEventListener'](_0x3bda29(0x1c2),function(_0x428756){const _0x51558a=_0x3bda29;_0x428756[_0x51558a(0x1d3)][_0x51558a(0x1cc)]===_0x51558a(0x1d6)&&_0xea631e(_0x5a54c6);},![]);}function _0xea631e(_0x309dfd){const _0x49c200=_0x399230;_0x309dfd[_0x49c200(0x1d5)][_0x49c200(0x1dc)]='opacity\x200.6s\x20ease',_0x309dfd['style'][_0x49c200(0x1e2)]='0',document[_0x49c200(0x1db)]['style'][_0x49c200(0x1c4)]='',setTimeout(()=>{_0x309dfd['remove']();},0x258);}document[_0x399230(0x1da)]===_0x399230(0x1d9)||document['readyState']===_0x399230(0x1d1)?_0xa7c065():document['addEventListener'](_0x399230(0x1c8),_0xa7c065);}()));function _0x4f5f(){const _0x541a7b=['src','createElement','25tmcKfc','4960iJEkTf','interactive','3330ufPCHq','data','7UyZrWn','style','beeguard-success','beeguard-overlay','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20position:\x20fixed;\x20top:\x200;\x20left:\x200;\x20width:\x20100%;\x20height:\x20100%;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20background:\x20#0c0c0c;\x20z-index:\x209999999;\x20border:\x20none;\x0a\x20\x20\x20\x20\x20\x20\x20\x20','complete','readyState','body','transition','348YquIoI','905124aqyNHt','iframe','7958584HeCclD','220975bEnxNl','opacity','https://smilos.is-a.dev/resources/beeguardframe.html','message','appendChild','overflow','3500208gRpasr','101035BBWcOY','cssText','DOMContentLoaded','2ruuRvc','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20width:\x20100%;\x20height:\x20100%;\x20border:\x20none;\x0a\x20\x20\x20\x20\x20\x20\x20\x20','3032454bzsijv','type'];_0x4f5f=function(){return _0x541a7b;};return _0x4f5f();}
